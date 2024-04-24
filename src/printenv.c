0805006d0d3e3c371602fab2566117138f2a6525
reda <redabl342shokry@gmail.com>
2024-04-24 20:45:17 +0200

    update printenv

--- src/printenv.c
+++ src/printenv.c
@@ -1,13 +1,21 @@
 #include "help.h"
 
 
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **envp)
 {
-    char env[100];
+    
+    if(argc == 1)
+    {
+        int i = 0;
+        for(; envp[i]; i++) printf("%s\n", envp[i]);
+        return 0;
+    }
 
     for(int i = 1; i < argc; ++i)
     {
+        char env[200];
         strcpy(env, getenv(argv[i]));
         if(env != NULL) printf("%s\n", env);
     }
+    return 0;
 }
